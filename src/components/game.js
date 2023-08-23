6f16e631d37fd0ac10f824ba9f90ee71e792a6ba
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-08-23 12:36:08 +0200

    Update game.js

--- src/components/game.js
+++ src/components/game.js
@@ -53,14 +53,18 @@ class Game extends React.Component {
     return (
       <SafeAreaView style>
         <View style={styles.container}>
-          <Text style={styles.target}>{this.target}</Text>
+          <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
+            {this.target}
+          </Text>
           <View style={styles.randomContainer}>
             {this.randomNumbers.map((randomNumber, index) => (
               <RandomNumber
                 key={index}
                 id={index}
                 number={randomNumber}
-                isDisabled={this.isNumberSelected(index)}
+                isDisabled={
+                  this.isNumberSelected(index) || gameStatus !== 'PLAYING'
+                }
                 onPress={this.selectNumber}
               />
             ))}
@@ -80,7 +84,6 @@ const styles = StyleSheet.create({
   },
   target: {
     fontSize: 40,
-    backgroundColor: '#aaa',
     marginHorizontal: 50,
     textAlign: 'center',
     marginTop: 30,
@@ -93,6 +96,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     width: '80%',
     marginTop: 50,
+  },
+  STATUS_PLAYING: {
+    backgroundColor: '#aaa',
+  },
+  STATUS_WON: {
+    backgroundColor: 'green',
+  },
+  STATUS_LOST: {
+    backgroundColor: 'red',
   },
 });
 
